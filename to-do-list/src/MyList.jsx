65a770e1856bd8703617d96952c03c40b7878804
Scuxe <scuxe547@gmail.com>
2025-07-09 19:14:24 +0530

    Completed the body of project. CSS pending

--- to-do-list/src/MyList.jsx
+++ to-do-list/src/MyList.jsx
@@ -5,20 +5,39 @@ function MyList(){
     const [task, setTask] = useState(["Eat Breakfast", "Take a shower", "Walk the dog"]);
     const [newTask, setNewTask] = useState("");
 
-    function handleInputChange(){
-        setNewTask(element.target.value);
+    function handleInputChange(event){
+        setNewTask(event.target.value);
     }
 
     function addTask(){
-        setTask(t => [...t, newTask]);
+        if(newTask.trim() !== "")
+        {
+            setTask(t => [...t, newTask]);
+            setNewTask("");
+        }
     }
 
-    function moveUp(){
-
+    function deleteTask(index)
+    {
+        setTask(task.filter((_, i) => i !== index));
     }
 
-    function moveDown(){
+    function moveUp(index){
+        if(index>0)
+        {
+            const updatedTasks = [...task];
+            [updatedTasks[index], updatedTasks[index-1]] = [updatedTasks[index-1], updatedTasks[index]]
+            setTask(updatedTasks);
+        }
+    }
 
+    function moveDown(index){
+        if(index<task.length-1)
+        {
+            const updatedTasks = [...task];
+            [updatedTasks[index], updatedTasks[index+1]] = [updatedTasks[index+1], updatedTasks[index]]
+            setTask(updatedTasks);
+        } 
     }
 
     return(<>
@@ -28,11 +47,21 @@ function MyList(){
             <input type='text'
             placeholder='Enter a task'
             value={newTask}
-            onChange={()=>handleInputChange}/>
+            onChange={() => handleInputChange(event)}
+            />
         </div>
         <button className='Add-button' 
         onClick={addTask}>Add Task</button>
-        
+        <ol>
+            {task.map((element, index) => 
+            <li key={index}>
+                <span>{element}</span>
+                <button className='deletebtn' onClick={() => deleteTask(index)}>Delete</button>
+                <button onClick={() => moveUp(index)}>☝️</button>
+                <button onClick={() => moveDown(index)}>👇</button>
+                </li>)}
+
+        </ol>
     </div>
     </>);
 
